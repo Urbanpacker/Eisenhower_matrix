e28e38b474ec0f694724e454212dfb3a8c10e920
Urbanpacker <dev@urbanities.fr>
2020-06-23 11:08:57 +0200

    Ajout styles pour bouton d'annulation de modification d'une tache

--- styles/config.rb
+++ styles/config.rb
@@ -5,11 +5,11 @@ require 'compass/import-once/activate'
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+images_dir = "../images"
+javascripts_dir = "../js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :expanded
+output_style = :compressed
 #or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
